d117689052692d261d5674ce9b22df007641472b
nikonst <nikonst@otenet.gr>
2025-09-12 08:08:48 +0300

    Update Header layout

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,23 +5,22 @@ import { Navbar, Nav, Button, NavDropdown } from 'react-bootstrap';
 function Header({ onLogout }) {
   return (
     <Navbar bg="light" variant="light" sticky="top" className="px-4 justify-content-between">
-      <Navbar.Brand>
-        <h1 className="mb-0">Eliza</h1>
-      </Navbar.Brand>
-      <NavDropdown title="Κατηγορίες" id="basic-nav-dropdown">
-        <NavDropdown.Item href="#">
-          Πωλήσεις
-        </NavDropdown.Item>
-        <NavDropdown.Item href="#" disabled>
-          Second option
-        </NavDropdown.Item>
-        <NavDropdown.Item href="#" disabled>
-          Third option
-        </NavDropdown.Item>
-      </NavDropdown>
+      {/* Left side */}
+      <div className="d-flex align-items-center gap-4">
+        <Navbar.Brand>
+          <h1 className="mb-0">Eliza</h1>
+        </Navbar.Brand>
+        <NavDropdown title="Κατηγορίες" id="basic-nav-dropdown">
+          <NavDropdown.Item href="#">Πωλήσεις</NavDropdown.Item>
+          <NavDropdown.Item href="#" disabled>Second option</NavDropdown.Item>
+          <NavDropdown.Item href="#" disabled>Third option</NavDropdown.Item>
+        </NavDropdown>
+      </div>
+
+      {/* Right side */}
       <Nav>
         <Button variant="danger" onClick={onLogout}>
-          Απσύνδεση
+          Αποσύνδεση
         </Button>
       </Nav>
     </Navbar>
