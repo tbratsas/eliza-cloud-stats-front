a9fa9d9da33cba924f3623cf5f6c1f36e6ab2ec0
nikonst <nikonst@otenet.gr>
2025-09-27 10:36:01 +0300

    Update env production and config ts

--- config.ts
+++ config.ts
@@ -1,3 +1,3 @@
 export const API_URL = import.meta.env.VITE_API_URL ?? "http://localhost:5001/api";
-export const APP_NAME = "Greek Sales App";
+export const APP_NAME = "Eliza Reports";
 export const IS_PRODUCTION = import.meta.env.MODE === "production";
